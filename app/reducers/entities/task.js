97ef84b582003e7db4c65923ef600701c9ef509d
seales <cseales@fastmail.com>
2017-09-16 21:11:36 -0400

    Fix task queue bug

--- app/reducers/entities/task.js
+++ app/reducers/entities/task.js
@@ -50,12 +50,12 @@ function addPendingTaskCreate(state, action) {
     TaskQueue.queueTaskCreate(action.task);
   } catch (err) {
     /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
   }
 
   return updateObject(state, {
@@ -85,12 +85,12 @@ function addPendingTaskUpdate(state, action) {
       TaskQueue.queueTaskCreate(action.task);
     } catch (err) {
       /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
     }
 
     updatedPendingTaskActions = {
@@ -100,13 +100,13 @@ function addPendingTaskUpdate(state, action) {
     };
   } else {
     /*
-       This block handles the following cases:
-       1. The task is already queued to be deleted
-       2. The task is already queued to be updated
-       3. The task is not queued for anything
+        This block handles the following cases:
+        1. The task is already queued to be deleted
+        2. The task is already queued to be updated
+        3. The task is not queued for anything
 
-       For all these scenarios, we want to queue an update.
-    */
+        For all these scenarios, we want to queue an update.
+     */
 
     let queuedUpdates = updateObject(
       state.pendingTaskActions.update,
@@ -117,12 +117,12 @@ function addPendingTaskUpdate(state, action) {
       TaskQueue.queueTaskUpdate(action.task);
     } catch (err) {
       /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
     }
 
     updatedPendingTaskActions = {
@@ -154,15 +154,15 @@ function addPendingTaskDelete(state, action) {
     }
 
     try {
-      TaskQueue.dequeueTaskByTaskId(action.task.id);
+      TaskQueue.dequeueTaskByTaskId(taskId);
     } catch (err) {
       /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
     }
 
     updatedPendingTaskActions = {
@@ -175,12 +175,12 @@ function addPendingTaskDelete(state, action) {
     taskId in state.pendingTaskActions.update
   ) {
     /*
-     If the task is not queued to be deleted, queue it now.
+      If the task is not queued to be deleted, queue it now.
 
-     If the task is already queued to be updated; queue it for deletion anyways,
-     because the backend design is such that deletes and updates do not conflict
-     with each other.
-    */
+      If the task is already queued to be updated; queue it for deletion anyways,
+      because the backend design is such that deletes and updates do not conflict
+      with each other.
+     */
     let queuedDeletes = updateObject(
       state.pendingTaskActions.delete,
       newTaskEntry
@@ -190,12 +190,12 @@ function addPendingTaskDelete(state, action) {
       TaskQueue.queueTaskDelete(action.task);
     } catch (err) {
       /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
     }
 
     updatedPendingTaskActions = {
@@ -219,6 +219,8 @@ function removePendingTaskCreate(state, action) {
     action.taskId
   );
 
+  TaskQueue.dequeueTaskByTaskId(action.taskId);
+
   // TODO - we must also update update / delete now that we have
   // the REAL task ID
 
@@ -233,30 +235,30 @@ function removePendingTaskCreate(state, action) {
   // TODO - refine the approach of replacing the existing task
 
   /*
-     Each task has a unique ID. This identifier is usually assigned by the
-     server. However, if for some reason, the client is unable to reach the
-     server, the client will create a temporary ID and then queue the task
-     for creation.
+      Each task has a unique ID. This identifier is usually assigned by the
+      server. However, if for some reason, the client is unable to reach the
+      server, the client will create a temporary ID and then queue the task
+      for creation.
 
-     Later, when the client can finally reach the server, the server will give
-     the task a new ID. Here we replace the old, client-assigned ID with the new,
-     server-assigned ID.
+      Later, when the client can finally reach the server, the server will give
+      the task a new ID. Here we replace the old, client-assigned ID with the new,
+      server-assigned ID.
 
-     Three places must be checked
-     1. state.tasks
-         --- this is where all tasks live
-     2. state.pendingTaskActions.update
-         --- unlikely but possible the task is also queued for an update
-     3. state.pendingTaskActions.delete
-         --- unlikely but possible the task is also queued for deletion
-   */
+      Three places must be checked
+      1. state.tasks
+          --- this is where all tasks live
+      2. state.pendingTaskActions.update
+          --- unlikely but possible the task is also queued for an update
+      3. state.pendingTaskActions.delete
+          --- unlikely but possible the task is also queued for deletion
+    */
   let task = Object.assign({}, state.tasks[clientAssignedTaskId]);
   task.id = serverAssignedTaskId;
 
   /*
-     Keep a reference to the clientAssignedTaskId in case a local reference
-     exists. TODO - refine this approach.
-   */
+      Keep a reference to the clientAssignedTaskId in case a local reference
+      exists. TODO - refine this approach.
+    */
   task.clientAssignedTaskId = clientAssignedTaskId;
   delete state.tasks[clientAssignedTaskId]; // delete existing task
   state.tasks[serverAssignedTaskId] = task; // replace with new ID
@@ -271,12 +273,12 @@ function removePendingTaskCreate(state, action) {
       TaskQueue.queueTaskUpdate(task);
     } catch (err) {
       /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
     }
   }
 
@@ -290,12 +292,12 @@ function removePendingTaskCreate(state, action) {
       TaskQueue.queueTaskDelete(task);
     } catch (err) {
       /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
     }
   }
 
@@ -323,12 +325,12 @@ function removePendingTaskUpdate(state, action) {
     TaskQueue.dequeueTaskByTaskId(action.taskId);
   } catch (err) {
     /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
   }
 
   return updateObject(state, {
@@ -355,12 +357,12 @@ function removePendingTaskDelete(state, action) {
     TaskQueue.dequeueTaskByTaskId(action.taskId);
   } catch (err) {
     /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
   }
 
   return updateObject(state, {
@@ -426,12 +428,12 @@ function deleteAllTasks(state, action) {
     TaskStorage.cleanTaskStorage();
   } catch (err) {
     /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
   }
 
   return updateObject(state, {
@@ -458,12 +460,12 @@ function deleteTask(state, action) {
     TaskStorage.deleteTaskByTaskId(action.taskId);
   } catch (err) {
     /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
   }
 
   return updateObject(state, { tasks: taskMap });
@@ -479,12 +481,12 @@ function addTasks(state, action) {
     TaskStorage.createOrUpdateTasks(action.tasks);
   } catch (err) {
     /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
   }
 
   return updateObject(state, {
@@ -504,12 +506,12 @@ function addNormalizedTask(state, normalizedTask) {
     TaskStorage.createOrUpdateTask(normalizedTask);
   } catch (err) {
     /*
-    If an error occurs when writing to disk, ignore it. Disk storage is a
-    non-critical feature, unlike the rest of the code here. We never want to
-    throw an error from a reducer.
+     If an error occurs when writing to disk, ignore it. Disk storage is a
+     non-critical feature, unlike the rest of the code here. We never want to
+     throw an error from a reducer.
 
-    TODO - reevaluate
-  */
+     TODO - reevaluate
+   */
   }
 
   return updateObject(state, {
@@ -518,10 +520,10 @@ function addNormalizedTask(state, normalizedTask) {
 }
 
 /*
-   This function always updates the value lastSuccessfulSyncDateTimeUtc. This is
-   because it is assumed that this function is ONLY invoked after a successful
-   sync.
- */
+    This function always updates the value lastSuccessfulSyncDateTimeUtc. This is
+    because it is assumed that this function is ONLY invoked after a successful
+    sync.
+  */
 function syncTasks(state, action) {
   const syncedTasks = action.tasks;
   const existingTasks = state.tasks;
@@ -547,13 +549,13 @@ function syncTasks(state, action) {
         tasksToCreateOrUpdate.push(syncedTask);
       } else {
         /*
-           The synced task was less up-to-date than the version residing on the
-           client. This is expected in some scenarios, such as when the client
-           looses network connectivity, and must queue up a task action.
+            The synced task was less up-to-date than the version residing on the
+            client. This is expected in some scenarios, such as when the client
+            looses network connectivity, and must queue up a task action.
 
-           For this case, we do nothing here. The queue-logic is designed to
-           completely handle such scenarios.
-         */
+            For this case, we do nothing here. The queue-logic is designed to
+            completely handle such scenarios.
+          */
       }
     } else {
       // synced task does not already exist on this device.
@@ -569,13 +571,13 @@ function syncTasks(state, action) {
 }
 
 /*
-   This method is not expected to always be correct. There are many nuances
-   involved with correctly syncing and queueing state, especially as more
-   clients are involved and the network is assumed to be unreliable.
+    This method is not expected to always be correct. There are many nuances
+    involved with correctly syncing and queueing state, especially as more
+    clients are involved and the network is assumed to be unreliable.
 
-   The current approach is to simply return false if the synced task was updated
-   at a LESS RECENT date than the task on the client.
- */
+    The current approach is to simply return false if the synced task was updated
+    at a LESS RECENT date than the task on the client.
+  */
 function syncedTaskDoesNotConflictWithQueuedTask(state, syncedTask) {
   let pendingTaskActions = state.pendingTaskActions;
 
@@ -647,83 +649,83 @@ const initialState = {
 function tasksReducer(state = initialState, action) {
   switch (action.type) {
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case START_QUEUED_TASK_SUBMIT:
       return startQueuedTaskSubmit(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case STOP_QUEUED_TASK_SUBMIT:
       return stopQueuedTaskSubmission(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case START_TASK_SYNC:
       return startTaskSync(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case END_TASK_SYNC:
       return endTaskSync(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case START_TASK_CLEANUP:
       return startTaskCleanup(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case STOP_TASK_CLEANUP:
       return stopTaskCleanup(state, action);
     /*
-      TODO - doc
-     */
+       TODO - doc
+      */
     case SYNC_TASKS:
       return syncTasks(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case CREATE_OR_UPDATE_TASK:
       return addTask(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case DELETE_ALL_TASKS:
       return deleteAllTasks(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case DELETE_TASK:
       return deleteTask(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case ADD_PENDING_TASK_DELETE:
       return addPendingTaskDelete(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case ADD_PENDING_TASK_UPDATE:
       return addPendingTaskUpdate(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case ADD_PENDING_TASK_CREATE:
       return addPendingTaskCreate(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case REMOVE_PENDING_TASK_DELETE:
       return removePendingTaskDelete(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case REMOVE_PENDING_TASK_UPDATE:
       return removePendingTaskUpdate(state, action);
     /*
-       TODO - doc
-     */
+        TODO - doc
+      */
     case REMOVE_PENDING_TASK_CREATE:
       return removePendingTaskCreate(state, action);
 
